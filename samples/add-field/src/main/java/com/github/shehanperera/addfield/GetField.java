d49cc38e02a892a9e83227d5ad32dd7d8dd756bc
ShehanPerera <12shehanakalanka@gmail.com>
2018-03-23 12:42:38 +0530

    Java Doc

--- samples/add-field/src/main/java/com/github/shehanperera/addfield/GetField.java
+++ samples/add-field/src/main/java/com/github/shehanperera/addfield/GetField.java
@@ -4,6 +4,10 @@ import net.bytebuddy.asm.Advice;
 
 public class GetField {
 
+    /**
+     * In this class we get the bool and word values before method2 execute
+     * method2 should be non-static method.
+     */
     @Advice.OnMethodEnter
     public static void exit(@Advice.FieldValue(value = "bool") boolean bool,
                             @Advice.FieldValue(value = "word") String word)
