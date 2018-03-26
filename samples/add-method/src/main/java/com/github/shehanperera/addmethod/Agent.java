2c53673f18e3724c6e871cce84c65519d2c426c8
ShehanPerera <12shehanakalanka@gmail.com>
2018-03-26 09:50:14 +0530

    Fix errors

--- samples/add-method/src/main/java/com/github/shehanperera/addmethod/Agent.java
+++ samples/add-method/src/main/java/com/github/shehanperera/addmethod/Agent.java
@@ -3,6 +3,7 @@ package com.github.shehanperera.addmethod;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.description.modifier.Visibility;
 import net.bytebuddy.implementation.MethodCall;
+import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.SuperMethodCall;
 import net.bytebuddy.matcher.ElementMatchers;
 
@@ -15,15 +16,17 @@ public class Agent {
 
     public static void premain(String arguments, Instrumentation instrumentation) {
 
-        System.out.println("Agent for Field Adding ");
+        System.out.println("Agent for add a method ");
 
         new AgentBuilder.Default()
                 .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
                 .type((ElementMatchers.nameContains("Method")))
                 .transform((builder, typeDescription, classLoader, module) -> builder
                         .defineMethod("method3", void.class, Visibility.PUBLIC)
+                        .intercept(MethodDelegation.to(AddMethod.class))
+                        .method(ElementMatchers.nameContains("method2"))
                         .intercept(SuperMethodCall.INSTANCE
-                                .andThen(MethodCall.invoke(ElementMatchers.named("sayHelloAgain"))))
+                                .andThen(MethodCall.invoke(ElementMatchers.nameContains("method3"))))
                 ).installOn(instrumentation);
 
     }
