d0ba0605c0e4c473f785f3165122502efeaf8708
ShehanPerera <12shehanakalanka@gmail.com>
2018-03-26 09:52:01 +0530

    Rearrange

--- samples/example-run/src/main/java/com/github/shehanperera/example/Sample.java
+++ samples/example-run/src/main/java/com/github/shehanperera/example/Sample.java
@@ -8,9 +8,11 @@ public class Sample {
     public static void main(String[] args) {
 
         System.out.println("This is Sample main");
+
         Method method = new Method();
         method.method1();
         method.method2();
         method.method4(4, "parameters", true);
+
     }
 }
